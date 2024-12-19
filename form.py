0c882816acfc3e509cc57b0dc6ddd05449cff77c
ddpavlov <ddpavlov@sigma-it.ru>
2024-12-19 17:45:44 +0300

    Add search for project field

--- form.py
+++ form.py
@@ -1,11 +1,14 @@
 from flask_wtf import FlaskForm
-from wtforms import SelectMultipleField, SelectField, StringField, SubmitField
+from wtforms import SelectMultipleField, SelectField, StringField, SubmitField, validators
 
 
 class GetReportForm(FlaskForm):
+    """"main UI form"""
     url = StringField('URL')
     token = StringField('Token')
-    project = StringField('Project')
+    project = SelectField("Project",
+        choices=[("", "")],
+        validators=[validators.InputRequired()])
     severities = SelectMultipleField('Severities (choose with "CTRL")',
         choices=[('critical', 'Critical'), ('high', 'High'), ('medium', 'Medium'), ('low', 'Low'), ('unassigned', 'Unassigned')],
         default=['critical', 'high'])
